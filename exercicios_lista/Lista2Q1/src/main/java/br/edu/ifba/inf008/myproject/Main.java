455d1219c51c1699987ff31c619b04a2f412887d
“Valder <mat.dossantos1@gmail.com>
2025-12-01 17:00:37 -0300

    exercicios

--- exercicios_lista/Lista2Q1/src/main/java/br/edu/ifba/inf008/myproject/Main.java
+++ exercicios_lista/Lista2Q1/src/main/java/br/edu/ifba/inf008/myproject/Main.java
@@ -4,7 +4,27 @@ package br.edu.ifba.inf008.myproject;
 public class Main {
     public static void main(String[] args) {
 
-        System.out.printf("Hello and welcome!");
+        /*int[] c = new int[12];
+
+        System.out.printf("    %s   %s%n", "Index", "Value");
+
+        for (int i = 0; i < c.length; i++){
+            System.out.printf("%5d%8d%n", i, c[i]);
+        }
+        */
+
+        final int ARRAY_LENGTH = 10;
+        int[] arr = new int[ARRAY_LENGTH];
+
+        for (int i = 0; i < arr.length; i++){
+            arr[i] = 2 + 2 * i;
+        }
+
+        System.out.printf("    %s   %s%n", "Index", "Value");
+
+        for (int i = 0; i < arr.length; i++){
+            System.out.printf("%5d%8d%n", i, arr[i]);
+        }
 
 
     }
--- exercicios_lista/Lista2Q1/src/main/java/br/edu/ifba/inf008/myproject/teste.java
+++ exercicios_lista/Lista2Q1/src/main/java/br/edu/ifba/inf008/myproject/teste.java
@@ -1,4 +1,6 @@
 package br.edu.ifba.inf008.myproject;
 
 public class teste {
+
+
 }
