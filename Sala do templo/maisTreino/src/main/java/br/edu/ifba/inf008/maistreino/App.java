bc76441151d07a6ab0b31e3693df51d0f90d838c
“Valder <mat.dossantos1@gmail.com>
2026-01-26 16:55:13 -0300

    pasta de testes

---
+++ Sala do templo/maisTreino/src/main/java/br/edu/ifba/inf008/maistreino/App.java
@@ -0,0 +1,10 @@
+package br.edu.ifba.inf008.maistreino;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
