9766117b0df9083e3ea65b1c13dbb2dffd50c6b2
valdersluz <mat.dossantos1@gmail.com>
2026-02-19 09:33:40 -0300

    novos testes

---
+++ Sala do templo/Sem Maven/Welcome1.java
@@ -0,0 +1,11 @@
+// Teste do livro de Deitel: Java, como programar
+
+
+public class Welcome1
+{
+    public static void main(String[] args)
+    {
+        System.out.print("Bem-vindo ao");
+        System.out.println("Primeiro programa em Java!");
+    }
+} 
