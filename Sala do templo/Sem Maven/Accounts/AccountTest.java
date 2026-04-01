d6876b2488f3eb71762238284cdb1cb0dc944980
“Valder <mat.dossantos1@gmail.com>
2026-04-01 16:56:38 -0300

    envio de testes livro deitel

---
+++ Sala do templo/Sem Maven/Accounts/AccountTest.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class AccountTest
+{
+    public static void main(String args[])
+    {
+        Account account1 = new Account("Jane Green, 52.00");
+        Account account2 = new Account("John Blue", -7.53);
+
+        System.out.printf("%s balance: $%.2f %n", account1.getName(), account1.getBalance());
+        System.out.printf("%s balance: $%.2f %n%n", account2.getName(), account2.getBalance());
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Entre o valor para depósito para conta 1: ");
+    }
+
+
+}
---
+++ Sala do templo/Sem Maven/Costr.java
@@ -0,0 +1,17 @@
+class Costr{
+	String nome;
+	int num;
+	float decim;
+	boolean is;
+	
+	public static void main(String args[]){
+		
+		Costr c = new Costr();
+		System.out.println("Nome sem iniciar construtor: " + c.nome);
+		System.out.println("num sem iniciar construtor: " + c.num);
+		System.out.println("decim sem iniciar construtor: " + c.decim);
+		System.out.println("is sem iniciar construtor: " + c.is);
+	}
+}
+
+
